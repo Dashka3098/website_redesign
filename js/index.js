b40de9d49f9c0e0104c5d3765e0a26fe43f417f9
Dashka3098 <dashka3098@gmail.com>
2021-10-12 09:51:05 +0300

    add media query

--- js/index.js
+++ js/index.js
@@ -1,11 +1,21 @@
 const menuItems = document.getElementsByClassName("menu_items");
 
-const item = menuItems[0].getElementsByClassName("item");
+const items = menuItems[0].getElementsByClassName("item");
 
-for (var i = 0; i < item.length; i++) {
-  item[i].addEventListener('click', function() {
+for (let item of items) {
+  item.addEventListener('click', function() {
     const current = document.getElementsByClassName('active');
     current[0].classList.remove('active');
     this.classList.add('active');
   });
-} 
+}
+
+function openNav() {
+  document.getElementById('sidebar').style.width = '270px';
+  document.getElementById('main').style.marginLeft = '270px';
+}
+
+function closeNav() {
+  document.getElementById('sidebar').style.width = '0';
+  document.getElementById('main').style.marginLeft= '0';
+}
