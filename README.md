deac54c9d6d9a6a1c54479e697091eca784ed292
Dashka3098 <dashka3098@gmail.com>
2021-10-13 20:15:20 +0300

    change before icon and fixed sidebar

---
+++ README.md
@@ -0,0 +1,3 @@
+**Website Redesign**
+
+Link: https://dashka3098.github.io/website_redesign/
--- js/index.js
+++ js/index.js
@@ -3,20 +3,37 @@ const menuItems = document.getElementsByClassName("menu_items");
 const items = menuItems[0].getElementsByClassName("item");
 
 for (let item of items) {
-  item.addEventListener('click', function() {
-    const current = document.getElementsByClassName('active');
-    current[0].classList.remove('active');
-    this.classList.add('active');
-  });
+	item.addEventListener('click', function () {
+		const current = document.getElementsByClassName('active');
+		current[0].classList.remove('active');
+		this.classList.add('active');
+	});
 }
 
+document.getElementById('burger').addEventListener('click', function (){
+	if (document.getElementById('sidebar').style.width === '270px'){
+		closeNav();
+	}else {
+		openNav();
+	}
+})
+
 function openNav() {
-  document.getElementById('sidebar').style.width = '270px';
-  document.getElementById('main').style.marginLeft = '270px';
+	document.getElementById('sidebar').style.width = '270px';
+	document.getElementById('main').style.marginLeft = '270px';
 }
 
 function closeNav() {
-  document.getElementById('sidebar').style.width = '0';
-  document.getElementById('main').style.marginLeft= '0';
+	document.getElementById('sidebar').style.width = '0';
+	document.getElementById('main').style.marginLeft = '0';
+
 }
+
+window.addEventListener('resize', function (event) {
+	if (window.screen.width > 768) {
+		openNav();
+	}else {
+		closeNav();
+	}
+});
 
